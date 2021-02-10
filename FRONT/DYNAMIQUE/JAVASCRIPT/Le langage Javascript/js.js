e75d1db38d33d1662904dad2702934d62ac24759
Noe-Kisungu <noekisungu@gmail.com>
2021-02-10 16:52:31 +0100

    Ajout js.js et Màj Phase 5

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/Le langage Javascript/js.js
@@ -0,0 +1,17 @@
+// Javascript
+var a;
+ 
+a = parseInt(prompt("6+6"));
+
+
+if (a==6) { // Comparaison
+    alert("Bien essayé !");
+}
+
+else if (a!=12) {
+    alert("Dommage !");
+}
+
+else {
+    alert("correct !");
+}
