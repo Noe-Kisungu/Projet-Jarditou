89372f481cf85c24572cb571060b79f83c76224c
Noe-Kisungu <noekisungu@gmail.com>
2021-02-17 16:57:11 +0100

    ajout evaluation

---
+++ FRONT/DYNAMIQUE/JAVASCRIPT/Evaluation/Js validation/form.js
@@ -0,0 +1,107 @@
+var zipc = new RegExp(/^[0-9]*$/);
+var lettre = new RegExp(/^[A-Za-z]+$/);
+var  mixt = new RegExp 
+// var lettre = new RegExp (/^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/);
+function inspect(formulaire)//prend le formulaire par son `nom '()'
+{// declaration des variable (sans oublier value)
+  var nom = document.getElementById("nom").value;
+  var postal = document.getElementById("postal").value;
+  var address = document.getElementById("address").value;
+  var ville = document.getElementById("ville").value;
+  var prenom = document.getElementById("prenom").value;
+  var Bool = true;
+
+  if (nom == "")// si le champ est vide : affichage d'un message de notification
+  {
+    document.getElementById('nomerr').innerHTML = "le champ est vide!";
+    document.getElementById('nomerr').style.color = "red"; //
+    Bool = false;
+    document.getElementById("nom").focus();
+  }
+  else if (!lettre.test(nom)) {
+    document.getElementById('nomerr').style.color = "red";
+    document.getElementById("nomerr").innerHTML = "Ne correspond pas au bon format";
+    Bool = false;
+    document.getElementById("nom").focus();
+  }
+  else {
+    document.getElementById("nomerr").innerHTML = "";
+  }
+
+
+  if (prenom == "") {
+    document.getElementById('prenomerr').style.color = "red";
+    document.getElementById('prenomerr').innerHTML = "le champ est vide!";
+    Bool = false;
+    document.getElementById("prenom").focus();
+  }
+  else if (!lettre.test(prenom)) {
+    document.getElementById('prenomerr').style.color = "red";
+    document.getElementById("prenomerr").innerHTML = 'Ne correspond pas au bon format';
+    Bool = false;
+    document.getElementById("prenom").focus();
+  }
+  else {
+    document.getElementById("prenomerr").innerHTML = '';
+  }
+
+  if (postal == "") {
+    document.getElementById('postalerr').style.color = "red";
+    document.getElementById('postalerr').innerHTML = "le champ est vide!";
+    Bool = false;
+    document.getElementById("postal").focus();
+  }
+  else if (!zipc.test(postal)) {
+    document.getElementById('postalerr').style.color = "red";
+    document.getElementById("postalerr").innerHTML = 'Ne correspond pas au bon format';
+    Bool = false;
+    document.getElementById("postal").focus();
+  }
+  else {
+    document.getElementById("postalerr").innerHTML = '';
+  }
+
+
+  if (address == "") {
+    document.getElementById('addresserr').style.color = "red";
+    document.getElementById('addresserr').innerHTML = "le champ est vide!";
+    Bool = false;
+    document.getElementById("address").focus();
+  }
+  else if (!lettre.test(address)) {
+    document.getElementById('addresserr').style.color = "red";
+    document.getElementById("addresserr").innerHTML = 'Ne correspond pas au bon format';
+    Bool = false;
+    document.getElementById("address").focus();
+  }
+  else {
+    document.getElementById("addresserr").innerHTML = '';
+  }
+
+
+  if (ville == "") {
+    document.getElementById('villerr').style.color = "red";
+    document.getElementById('villerr').innerHTML = "le champ est vide!";
+    Bool = false;
+    document.getElementById("ville").focus();
+  }
+  else if (!lettre.test(ville)) {
+    document.getElementById('villerr').style.color = "red";
+    document.getElementById("villerr").innerHTML = 'Ne correspond pas au bon format';
+    Bool = false;
+    document.getElementById("ville").focus();
+  }
+  else {
+    document.getElementById("villerr").innerHTML = '';
+  }
+
+
+  return Bool
+}
+
+
+
+
+/* email, radio, checkbox, date, sujet et bloc de texte= pas de regex (laisser bootstrap gérer les contraintes avec required)
+regex (enlever required)= nom, prenom, ville, adrrese, cp 
+ */
--- FRONT/DYNAMIQUE/JAVASCRIPT/Le langage Javascript/js.js
+++ FRONT/DYNAMIQUE/JAVASCRIPT/Le langage Javascript/js.js
@@ -1,4 +1,5 @@
 // Javascript
+ // -------LES CONDITION-------
 var a;
  
 a = parseInt(prompt("6+6"));
@@ -15,3 +16,62 @@ else if (a!=12) {
 else {
     alert("correct !");
 }
+
+// -------INCREMENTATION ET DECREMEMENTATION-------
+
+var number = 100;
+number = number + 4400;
+number++;// Affiche : « 1+ (Incremente l'element qui le precéde)»
+number--;// Affiche : « -1 »
+++number;// Affiche : « 1+ »
+alert(number);
+
+// -------EXEMPLE DE LA BOUCLE WHILE-------
+
+var nicks = '',
+nick,
+proceed = true;
+while (proceed) {
+nick = prompt('Entrez un prénom :');
+if (nick) {
+nicks += nick + ' '; // Ajoute le nouveau prénom ainsi qu'une
+// espace juste après
+} else {
+proceed = false; // Aucun prénom n'a été entré, donc on fait en
+// sorte d'invalider la condition
+}
+}
+alert(nicks); // Affiche les prénoms à la suite
+
+// -------EXEMPLE COMPLEXE ( ET RARE) DE LA BOUCLE WHILE-------
+
+var prenoms = '',
+prenom;
+while (true) {
+prenom = prompt('Entrez un prénom :');
+if (prenom) {
+prenoms += prenom + ' '; // Ajoute le nouveau prénom ainsi qu'une
+// espace juste après
+} else {
+break; // On quitte la boucle
+}
+}
+alert(prenoms); // Affiche les prénoms à la suite
+
+// -------EXEMPLE D'UNE BOUCLE WHILE D'INCREMENTATION-------
+for (var iter = 0; iter < 5; iter++) {
+    alert('Itération n°' + iter);
+    }
+
+// -------EXEMPLE D'UNE BOUCLE WHILE UTILISé SANS INCREMENTATION------- 
+
+for (var prenoms = '', prenom; true;) {
+    prenom = prompt('Entrez un prénom :');
+    if (prenom) {
+    prenoms += prenom + ' ';// Une histoire de chaine de caractère devenue booléens
+
+    } else {
+    break;
+    }
+    }
+    alert(prenoms);
