f99baa1fd5f4b6ef12309e565687176fc585861f
Noe-Kisungu <noekisungu@gmail.com>
2021-03-05 12:18:42 +0100

    PHP PDO-intro

---
+++ BACK/SERVEUR/PHP/C_wamp64_www_PHP/PDO-Intro.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO-Introduction</title>
+</head>
+<body>
+<img class="pdo-picture" src="https://bpesquet.gitbooks.io/guide-developpeur-php/content/partie1/images/acces-bd/archi-pdo.png" alt="schéma de fonctionnement du pdo">
+
+<?php 
+   class Personne //CLASS - une classe un moule grâce auquel nous allons créer autant d'objets de même type et de même structure qu'on le désire.
+   {
+  // ATTRIBUTS - Les « attributs » (aussi appelés « données membres ») sont les caractères propres à un objet (comme l'ADN).
+     public $nom;//En POO, un attribut n'est ni plus ni moins qu'une variable.
+     public $prenom;
+     public $dateDeNaissance;
+     public $taille;
+     public $sexe;
+  // Constantes
+     const NOMBRE_DE_BRAS = 2;
+     const NOMBRE_DE_JAMBES = 2;//une constante est déclarée avec le mot-clé const
+     const NOMBRE_DE_YEUX = 2;
+     const NOMBRE_DE_PIEDS = 2;
+     const NOMBRE_DE_MAINS = 2;
+  // METHODES - Les « méthodes » sont les "actions" que l'on peut appliquer à un objet. 
+     public function __construct() { }//declaration du constructeur. Les classes qui possèdent une méthode constructeur appellent cette méthode à chaque création d'une nouvelle instance de l'obj
+      
+     public function boire()
+    {
+      echo 'La personne boit<br/>';
+    }
+ 
+    public function manger()
+    {
+      echo 'La personne mange<br/>';
+    }//(=déclaré 5 attributs publics, 5 constantes, 1 méthode constructeur et 2 méthodes classiques)
+   }
+          ?> 
+</body>
+</html>
--- BACK/SERVEUR/PHP/C_wamp64_www_PHP/hello.php
+++ BACK/SERVEUR/PHP/C_wamp64_www_PHP/hello.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Initiation</title>
+    <title>Hello</title>
 </head>
 <body>
 <?php 
     /*Test*/ 
-      echo "Bonjour le monde"; 
+      echo "<h1>Bonjour le monde<h1>"; 
       echo "<h3> Aujourd'hui le ". date('d / M / Y H:m:s')."</ h3><hr />";
       echo "<h2>Bienvenue sur le site PHP 7</h2>";
       echo "<h4>La date et l’heure affichées ici sont celles du serveur et non pas celle de votre ordinateur(comme le ferait JavaScript.)</h4>";
 
-      echo "<h4>02 - Les variables et les types</h4>";
+      echo "<h4>02 - LES VARIABLES ET LES TYPES</h4>";
     //Les "variables variables"
       $var1 = "bonjour";
       $$var1 = "le monde"; 
@@ -84,21 +84,21 @@
          Test1(); 
          Test1();
          
-         echo "<h4>03 - Les opérateurs</h4>";
+         echo "<h4>03 - LES OPÉRATEURS</h4>";
          $a = "Winter"; 
          echo $a . " is coming !";
-         echo "<h4>02 - Les instructions conditionnelles</h4>";
+         echo "<h4>02 - LES INSTRUCTIONS CONDITIONNELLES</h4>";
        // Opérateur de condition ternaire
-         $age = 25; 
+         $age = 15; 
 
         (($age >= 18) ? $reponse="majeur" : $reponse="mineur"); 
 
-         echo "Vous êtes ".$reponse. ".";
+         echo "Vous êtes ".$reponse. "."."<br>";
        //condition ternaire plus concise  
-         $age = 25;
+         $age = 18;
          echo "Vous êtes ".(($age >= 18) ? "majeur" : "mineur"). ".";
          
-         echo "<h4>03 - Les boucles</h4>";
+         echo "<h4>03 - LES BOUCLES</h4>";
       //FOR -- L'instruction FOR reste plus compacte dans son code.
          for ($a = 1; $a < 10; $a++) { //$iInitialisation de variable; condition; incrémentation++
          echo $a."<br>"; //instructions
@@ -117,10 +117,81 @@
          echo "<br>".$a."<br>";;  
          } while ($a > 0); 
 
-          echo "<h4>05 - Les fonctions</h4>";
+          echo "<h4>05 - LES FONCTIONS</h4>";
+          // ...
+          echo "<h4>* - TÉLÉCHARGEMENT DE FICHIER EN PHP </h4>";
+          //https://www.youtube.com/watch?v=kkkQoGEzP0w
+          // ...
+          echo "<h4>* - LES TABLEAUX</h4>";
+      // Manipulation des tableaux associatifs
+          $facture = array("Janvier"=>500, "Février"=>620, "Mars"=>300, "Avril"=>130, "Mai"=>560, "Juin"=>350); 
+          $facture_sixmois=0; 
+      
+          foreach ($facture as $mois => $valeur) 
+          { 
+             echo "Facture du mois de $mois : $valeur Euros<br>"; 
+             $facture_sixmois +=$valeur; //ce qui fait $valeur+$valeur(on aditione le tout)
+          } //L'instruction foreach permet d'extraire un couple (clé, valeur) à chaque tour de boucle. 
+          echo "Facture total de six mois : <b>$facture_sixmois Euros</b><br>"; 
+       //***SORT - La fonction sort()
+           $nom = array("franck","laurent","caroline","magali","veronique");   
+           sort($nom);
 
+           for ($nb1=0;$nb1<=count($nom)-1; $nb1++) 
+           {
+           echo "$nom[$nb1]<br>";
+           }
+       //***RSORT (=desc)- La fonction rsort(array);
+           $nom = array("franck","laurent","caroline","magali","veronique");
+           rsort($nom);
+   
+           for ($nb1=0;$nb1<=count($nom)-1; $nb1++) 
+           {
+           echo "$nom[$nb1]<br>";
+           }
+       //***COUNT - La fonction count()
+            $tableau = array("Lundi","Mardi","Mercredi", "Jeudi", "Vendredi"); 
+            $nb = count($tableau); //Retourne le nombre d'éléments d'un tableau.
+            echo"Le tableau contient ".$nb." éléments."; //Affiche 5 (5 éléments dans le tableau).
+       //***ARRAY_PUSH() - La fonction array_push();
+            $tableau = array("Lundi","Mardi","Mercredi","Jeudi"); 
+            array_push($tableau, "Vendredi"); //Cette instruction permet d'ajouter un élément à la fin du tableau.
+            /******POUR LA SUITE? CE REFEREER AU CH08 SUR LES TABLEAUX********/
+            
+            echo "<h4>06 - LES FORMULAIRES</h4>";
+            //Pour eviter que les données soit affiché dans l'URL = not GET but POST!
 
-  
-  ?> 
+           // *****FONCTIONS UTILE********/
+
+          //empty — Détermine si une variable est vide
+            if (empty($_POST["nom"])) 
+            {
+                echo "Le nom doit être renseigné<br>";
+            }
+
+        //strlen — Calcule la taille d'une chaîne
+           $str1 = 'abcdef';
+            echo strlen($str1)."<br>"; // 6
+            //OU
+            $str2 = ' ab cd ';
+            echo strlen($str2)."<br>"; // 7
+        //preg_match()	Recherche si une chaîne de caractère est contenue dans une autre (ex. recherche si ABCDE contient BCD). Peut rechercher via des expressions régulières
+        
+            if (preg_match("/[BCD]/","ABCDEF"))
+            { 
+              echo "Trouvé<br>"; 
+            } 
+            else
+            {
+              echo "potrouvé";
+            }
+            //Renvoie 1 (TRUE) si touvé, 0 (FALSE) sinon
+        // isset — Détermine si une variable est déclarée et est différente de null
+        // Ceci est vrai, alors le texte est affiché
+           if (isset($var)); { echo 'Cette variable existe, donc je peux l\'afficher.'; } //Ceci est vrai, alors le texte est affiché
+
+             
+
+          ?> 
 </body>
 </html>
